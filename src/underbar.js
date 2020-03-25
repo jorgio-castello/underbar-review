8bc337a180a2da5fd160884927dbcd9cd1d9e51c
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-24 21:55:07 -0700

    Add logic for _.every
    1. Re-assign iterator to _.identity, if iterator is undefined
    as a parameter
    2. Reduce the collection, the passed in function checks
    whether the accumulator and value returned from invoking
    iterator on the current element both resolve to true
    2a. If both accumulator and iterator(element) are true,
    return true
    2b. If not, return false
    2c. We include a default accumulator of true so that
    the test can successfully begin by checking the
    return value of invoking iterator on the first element
    of the collection
    3. Return the boolean that is generated by reducing
    the collection

--- src/underbar.js
+++ src/underbar.js
@@ -218,10 +218,18 @@
     }, false);
   };
 
-
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    iterator = iterator || _.identity;
+
+    return _.reduce(collection, function(accumulator, item) {
+      if (accumulator && iterator(item)) {
+        return true;
+      } else {
+        return false;
+      }
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
